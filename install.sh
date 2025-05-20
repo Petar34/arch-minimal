cd2ce9d29d5a508f4d6469a1763bd030dfaccdf0
djivanovic <petar@djivanovic.com>
2025-05-20 11:59:52 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-echo "[INFO] Pokreće se automatska instalacija Arch Linuxa."
+echo -e "\033[1;36m[INFO] Pokreće se automatska instalacija Arch Linuxa...\033[0m"
+
+# Provjeri internet vezu
+ping -c 1 archlinux.org > /dev/null 2>&1
+if [[ $? -ne 0 ]]; then
+  echo "[GREŠKA] Nema internetske veze. Provjeri mrežu prije nastavka."
+  exit 1
+fi
 
 # Potvrda korisnika prije brisanja diska
 read -p "Upiši naziv diska (npr. /dev/sda ili /dev/nvme0n1): " DISK
@@ -25,7 +32,18 @@ mount ${DISK}2 /mnt
 mkdir /mnt/boot
 mount ${DISK}1 /mnt/boot
 
-# Instalacija base sistema + dodatni alati
+# (Opcionalno) Swap file – možeš obrisati ako ne trebaš
+fallocate -l 2G /mnt/swapfile
+chmod 600 /mnt/swapfile
+mkswap /mnt/swapfile
+swapon /mnt/swapfile
+echo "/swapfile none swap defaults 0 0" >> /mnt/etc/fstab
+
+# (Opcionalno) Update mirror liste – komentiraj ako ne koristiš
+# pacman -Sy reflector --noconfirm
+# reflector --latest 5 --sort rate --save /etc/pacman.d/mirrorlist
+
+# Instalacija base sistema + alati
 pacstrap /mnt base linux linux-firmware grub sudo networkmanager neovim \
 man-db man-pages base-devel
 
@@ -39,9 +57,9 @@ echo "petar" > /mnt/etc/hostname
 echo "LANG=hr_HR.UTF-8" > /mnt/etc/locale.conf
 ln -sf /usr/share/zoneinfo/Europe/Zagreb /mnt/etc/localtime
 
-# Kopiraj post-install u chroot
+# Preuzmi post-install.sh skriptu iz GitHub-a
 curl -o /mnt/root/post-install.sh https://raw.githubusercontent.com/Petar34/arch-minimal/main/post-install.sh
 chmod +x /mnt/root/post-install.sh
 
-# Chroot i pokreni postinstall
+# Pokreni post-install.sh unutar chroot okruženja
 arch-chroot /mnt /root/post-install.sh

afefca3944d0e037a0413bd5dccdacb3d308fa58
djivanovic <petar@djivanovic.com>
2025-05-20 20:05:47 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,11 +2,7 @@
 
 set -e
 
-echo -e "\033[1;36m[INFO] Osvježavam mirror listu...\033[0m"
-pacman -Sy reflector --noconfirm
-reflector --country "Germany,Croatia,Netherlands,Austria" --latest 10 --sort rate --save /etc/pacman.d/mirrorlist
-
-echo -e "\033[1;36m[INFO] Pokreće se automatska instalacija Arch Linuxa...\033[0m"
+echo -e "\033[1;36m[INFO] Pokreće se automatska instalacija Ubuntu Server 24.04...\033[0m"
 
 lsblk
 echo ""
@@ -14,7 +10,7 @@ read -p "Upiši naziv diska (npr. /dev/sda ili /dev/nvme0n1): " DISK
 read -p "[UPOZORENJE] SVI PODACI NA $DISK ĆE BITI OBRISANI. Nastavi? (da/ne): " potvrda
 [[ $potvrda != "da" ]] && echo "Prekinuto." && exit 1
 
-# Brisanje i kreiranje particija
+# Brisanje i priprema diska
 sgdisk --zap-all "$DISK"
 parted "$DISK" mklabel gpt
 parted "$DISK" mkpart ESP fat32 1MiB 513MiB
@@ -36,37 +32,39 @@ chmod 600 /mnt/swapfile
 mkswap /mnt/swapfile
 swapon /mnt/swapfile
 
-# Instalacija paketa
-pacstrap /mnt base linux linux-firmware grub efibootmgr sudo networkmanager neovim base-devel man-db man-pages curl git
+# Bootstrap minimalne instalacije (Ubuntu base system)
+debootstrap noble /mnt http://archive.ubuntu.com/ubuntu/
 
-# fstab
-mkdir -p /mnt/etc
-genfstab -U /mnt >> /mnt/etc/fstab
-
-# Lokalizacija
-echo "admin" > /mnt/etc/hostname
-echo "LANG=hr_HR.UTF-8" > /mnt/etc/locale.conf
-ln -sf /usr/share/zoneinfo/Europe/Zagreb /mnt/etc/localtime
-echo "en_US.UTF-8 UTF-8" >> /mnt/etc/locale.gen
-echo "hr_HR.UTF-8 UTF-8" >> /mnt/etc/locale.gen
-
-# Mountanje potrebnih datotečnih sustava
+# Mountanje potrebnih sustava
 for dir in proc sys dev run tmp; do
     mkdir -p /mnt/$dir
 done
 chmod 1777 /mnt/tmp
-mount --types proc /proc /mnt/proc
-mount --rbind /sys /mnt/sys && mount --make-rslave /mnt/sys
-mount --rbind /dev /mnt/dev && mount --make-rslave /mnt/dev
+mount --bind /dev /mnt/dev
+mount --bind /dev/pts /mnt/dev/pts
+mount --bind /proc /mnt/proc
+mount --bind /sys /mnt/sys
 mount --bind /run /mnt/run
 
-# Preuzimanje skripti
-curl -o /mnt/root/post-install.sh https://raw.githubusercontent.com/Petar34/arch-minimal/main/post-install.sh
-curl -o /mnt/root/post-i3.sh https://raw.githubusercontent.com/Petar34/arch-minimal/main/post-i3.sh
+# fstab
+UUID_ROOT=$(blkid -s UUID -o value "${DISK}p2")
+UUID_EFI=$(blkid -s UUID -o value "${DISK}p1")
+echo "UUID=$UUID_ROOT / ext4 defaults 0 1" >> /mnt/etc/fstab
+echo "UUID=$UUID_EFI /boot/efi vfat umask=0077 0 1" >> /mnt/etc/fstab
+echo "/swapfile none swap sw 0 0" >> /mnt/etc/fstab
+
+# Hostname i lokalizacija
+echo "admin" > /mnt/etc/hostname
+echo "LANG=hr_HR.UTF-8" > /mnt/etc/default/locale
+ln -sf /usr/share/zoneinfo/Europe/Zagreb /mnt/etc/localtime
+
+# Preuzimanje postinstall skripti
+curl -o /mnt/root/post-install.sh https://raw.githubusercontent.com/Petar34/ubuntu-minimal/main/post-install.sh
+curl -o /mnt/root/post-i3.sh https://raw.githubusercontent.com/Petar34/ubuntu-minimal/main/post-i3.sh
 chmod +x /mnt/root/post-install.sh /mnt/root/post-i3.sh
 
-# Chroot i pokretanje postavki
-arch-chroot /mnt /root/post-install.sh
-arch-chroot /mnt /root/post-i3.sh
+# Chroot i nastavak instalacije
+chroot /mnt /root/post-install.sh
+chroot /mnt /root/post-i3.sh
 
-echo -e "\n\033[1;32m[INFO] Instalacija završena! Pokreni reboot.\033[0m"
+echo -e "\n\033[1;32m[INFO] Ubuntu instalacija završena! Možeš pokrenuti reboot.\033[0m"

e6b67b1ebe1443c18eeaf2c1a8b393555b96c4d6
djivanovic <petar@djivanovic.com>
2025-05-20 11:26:52 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# Ovo je osnovni template. Dodaj potvrdu da korisnik zna što radi!
 echo "[INFO] Pokreće se automatska instalacija Arch Linuxa."
-read -p "Upiši naziv diska (npr. /dev/sda ili /dev/nvme0n1): " DISK
 
-# Brisanje postojećih particija (oprez!)
+# Potvrda korisnika prije brisanja diska
+read -p "Upiši naziv diska (npr. /dev/sda ili /dev/nvme0n1): " DISK
+read -p "[UPOZORENJE] SVI PODACI NA $DISK ĆE BITI OBRISANI. Nastavi? (y/n): " potvrda
+[[ $potvrda != "y" ]] && echo "Prekinuto." && exit 1
+
+# Brisanje postojećih particija
 sgdisk --zap-all $DISK
 
 # Kreiranje particija: EFI + root
@@ -22,12 +25,19 @@ mount ${DISK}2 /mnt
 mkdir /mnt/boot
 mount ${DISK}1 /mnt/boot
 
-# Instalacija base sistema
-pacstrap /mnt base linux linux-firmware grub sudo networkmanager neovim
+# Instalacija base sistema + dodatni alati
+pacstrap /mnt base linux linux-firmware grub sudo networkmanager neovim \
 man-db man-pages base-devel
 
 # Generiraj fstab
 genfstab -U /mnt >> /mnt/etc/fstab
+
+# Postavi hostname (možeš slobodno promijeniti ime)
+echo "petar" > /mnt/etc/hostname
+
+# Postavi lokalizaciju (hrvatski jezik i vremenska zona)
+echo "LANG=hr_HR.UTF-8" > /mnt/etc/locale.conf
+ln -sf /usr/share/zoneinfo/Europe/Zagreb /mnt/etc/localtime
 
 # Kopiraj post-install u chroot
 curl -o /mnt/root/post-install.sh https://raw.githubusercontent.com/Petar34/arch-minimal/main/post-install.sh

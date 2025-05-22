c8967422e86a7966762a7fd02430a52ceaf5c58d
djivanovic <petar@djivanovic.com>
2025-05-22 09:07:49 +0200

    Update post-i3.sh

--- post-i3.sh
+++ post-i3.sh
@@ -6,7 +6,7 @@ USERNAME=admin
 echo -e "\n\033[1;36m[INFO] Instalacija X servera, i3 i alata...\033[0m"
 apt update
 apt install -y xorg i3 i3status i3lock rofi feh picom lxappearance \
-  network-manager-gnome xterm
+  network-manager-gnome xterm xinit
 
 # Provjera postoji li home direktorij
 if [ ! -d "/home/$USERNAME" ]; then
@@ -23,7 +23,7 @@ mkdir -p /home/$USERNAME/.config/i3
 cat <<EOF > /home/$USERNAME/.config/i3/config
 exec --no-startup-id nm-applet
 exec --no-startup-id picom
-exec --no-startup-id feh --bg-scale /usr/share/backgrounds/ubuntu.png
+exec --no-startup-id feh --bg-scale /usr/share/pixmaps/debian-logo.png
 exec --no-startup-id setxkbmap hr
 EOF
 
